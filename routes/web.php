2cf92612fa60355967a2b98602a016e518963d7c
afisman <afisman@gmail.com>
2024-05-27 14:32:19 +0200

    added laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\RoomsController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\BookingController;
 
-use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [RoomsController::class, 'index'])->name('index');
@@ -26,4 +28,18 @@ Route::get('/roomDetails/{room}', [RoomsController::class, 'roomDetails'])->name
 Route::post('/bookingForm', [BookingController::class, 'store'])->name('bookingForm');
 
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
