5b1d00db17267844aa7f86733e30e9471a136552
afisman <afisman@gmail.com>
2024-06-06 12:16:28 +0200

    deploy with action

--- database/migrations/2024_05_28_084545_create_orders_table.php
+++ database/migrations/2024_05_28_084545_create_orders_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Schema::drop('orders');
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
